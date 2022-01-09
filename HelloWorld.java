86ad5575e8420f6106c27567191a28adc935707b
kadirivenkat123 <kadiri.venkat90@gmail.com>
2022-01-09 14:23:24 +0000

    added lines

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, vani good morning");
+    System.out.println("Hello, vani good morning");
+    System.out.println("Hello, vani good morning");
+    System.out.println("Hello, vani good morning");
+    System.out.println("Hello, vani good morning");
+    System.out.println("Hello, vani good morning");
+    System.out.println("Hello, vani good morning");
+    System.out.println("Hello, vani good morning");
+    System.out.println("Hello, vani good morning");
+    System.out.println("Hello, vani good morning");
+  }
+}
